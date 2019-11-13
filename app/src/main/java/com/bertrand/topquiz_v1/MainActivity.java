c80c9fff2de23063900a4048c55b9590e96323b1
Ratni Mohamed <mohmetal@live.fr>
2019-11-13 16:51:22 +0100

    firest commit for add TopQuiz_v1 of GitHub, this is a firest version V1

---
+++ app/src/main/java/com/bertrand/topquiz_v1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.bertrand.topquiz_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView refTextePrincipal; // @+id/activity_main_texte_principal
+    private EditText refInput;
+    private Button refBtnJouer, getRefBtnRaz;
+    private int compteur;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // création des Balises
+        refTextePrincipal = (TextView) findViewById(R.id.activity_main_texte_principal);
+        refInput = (EditText) findViewById(R.id.activity_main_input);
+        refBtnJouer = (Button) findViewById(R.id.activity_main_btn_jouer);
+
+
+        // initialisation
+        refBtnJouer.setEnabled(false);
+        getRefBtnRaz.setEnabled(false);
+
+
+
+        // création des listener
+        refInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().length() !=0 ){
+                    refBtnJouer.setEnabled(true);
+                }
+                else {
+                    refBtnJouer.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        refBtnJouer.setOnClickListener(new View.OnClickListener() {
+            private String pluriel='';
+            @Override
+            public void onClick(View v) {
+
+                compteur++;
+                getRefBtnRaz.setEnabled(true);
+
+
+            }
+        });
+
+    }
+}
